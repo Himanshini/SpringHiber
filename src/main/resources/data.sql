660447b2e7a335ae7e447fdd4f8d47e980504a0a
Himanshini <69730475+Himanshini@users.noreply.github.com>
2022-03-04 14:03:55 +0530

    Project using spring Hibernate

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+create table Emp
+(
+	id int not null,
+	name varchar(234) not null,
+	salary int,
+	designation varchar(234),
+	primary key(id)
+);
+
+
+insert into employee(id,name,salary,designation)values(1001,'sameer',80000,'Manager');
+insert into employee(id,name,salary,designation)values(1002,'sam',50000,'Devloper');
+insert into employee(id,name,salary,designation)values(1003,'rani',60000,'Tester');
+insert into employee(id,name,salary,designation)values(1004,'mahi',50000,'Accountant');
+insert into employee(id,name,salary,designation)values(1005,'hv',30000,'Clerk');
